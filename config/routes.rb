97f54702f7daf1572a791b304c03c7ec9e79fcd5
dhrudtjq03 <dhrudtjq03@gmail.com>
2021-12-08 13:04:22 +0000

    third

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :comments
   resources :episodes
   resources :webtoons
-	root 'buys#index'
+	#root 'buys#index'
+	root 'homes#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
